024eda51cd52056fe0d1231f2d436bb586d42af2
Yuchen Liu <43634213+hkulyc@users.noreply.github.com>
2022-06-22 01:47:54 +0800

    minor

--- src/clojask_io/input.clj
+++ src/clojask_io/input.clj
@@ -17,18 +17,28 @@
 
 (defn csv-local
   "read in a local csv dataset"
-  [path & {:keys [sep stat] :or {sep #"," stat false}}]
+  [path & {:keys [sep stat wrap] :or {sep #"," stat false wrap nil}}]
   (let [sep (if (string? sep) (re-pattern sep) sep)
         reader (io/reader path)
         data (line-seq reader)
-        data (map #(str/split % sep -1) data)]
+        data (map #(str/split % sep -1) data)
+        data (if (= wrap nil)
+               data
+               (let [wrap-len (count wrap)]
+                 (map #(map
+                        (fn [value]
+                          (let [value (if (and (str/ends-with? value wrap) (str/starts-with? value wrap))
+                                        (subs value wrap-len (- (count value) wrap-len))
+                                        value)]
+                            value))
+                        %) data)))]
     (if stat
       {:data (fn [] data) :size (.length (io/file path))}
       (fn [] data))))
 
 (defn csv-online
-  [path & {:keys [sep stat] :or {sep #"," stat false}}]
-  (let [data (csv-local path :sep sep :stat false)]
+  [path & {:keys [sep stat wrap] :or {sep #"," stat false wrap nil}}]
+  (let [data (csv-local path :sep sep :stat false :wrap wrap)]
     (if stat
       {:data data :size (get-online-size path)}
       data)))
@@ -42,12 +52,17 @@
 
 (defn read-file
   "Lazily read a dataset file into a vector of vectors"
-  [path & {:keys [sep format stat] :or {sep #"," format "txt" stat false}}]
-  (let [format (infer-format path)]
-    (if (.contains ["csv" "txt" "dat" nil] format)
+  [path & {:keys [sep format stat wrap] :or {sep #"," format nil stat false wrap nil}}]
+  (let [format (or format (infer-format path))]
+    (if (.contains ["piquet" "xls" "xlsx" "dta"] format)
+      ;; not supported type
       (do
-        (if (= format nil) (println "WARNING: The format of the file cannot be inferred. Use \"csv\" by default"))
+        (println (str "ERROR: The file type " format " is not supported."))
+        nil)
+      ;; ["csv" "txt" "dat" "tsv" "tab" nil]
+      (do
+        (if (or (= format nil) (not (.contains ["csv" "txt" "dat" "tsv" "tab"] format))) (println "WARNING: The format of the file cannot be inferred. Use \"csv\" by default"))
         (if (or (str/starts-with? path "https://") (str/starts-with? path "http://"))
-          (csv-online path :sep sep :stat stat)
-          (csv-local path :sep sep :stat stat)))))
+          (csv-online path :sep sep :stat stat :wrap wrap)
+          (csv-local path :sep sep :stat stat :wrap wrap)))))
   )
