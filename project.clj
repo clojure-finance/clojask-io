9ffe47ee9ecc18f82f060a98559b9f98d6d2176a
Yuchen Liu <43634213+hkulyc@users.noreply.github.com>
2022-07-09 16:34:08 +0800

    1.0.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.github.clojure-finance/clojask-io "1.0.1"
+(defproject com.github.clojure-finance/clojask-io "1.0.2"
   :description "A Clojure library designed to extend the file support for Clojask. This library can also be used alone to read in and output dataset files."
   :url "https://clojure-finance.github.io/clojask-website"
   :license {:name "MIT"
